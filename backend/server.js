1b7b5ab7d85e18c9ecd54179fd5d1aa0d4fbec9c
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-08-05 15:16:52 -0400

    created error middleware file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT;
 
 const app = express();
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
